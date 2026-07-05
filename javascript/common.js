39a6d66795e4d8a79de7c7086db5399ceecf22d3
qwewer0 <57561110+qwewer0@users.noreply.github.com>
2026-07-05 16:37:44 +0200

    Update thumbnailing service API URL

--- javascript/common.js
+++ javascript/common.js
@@ -68,7 +68,7 @@ function createDefaults() {
 		show_new_entry: "true",
 		show_options_gear: "true",
 		show_subfolder_icons: "true",
-		thumbnailing_service: "http://api.webthumbnail.org/?width=500&height=400&screen=1280&url=[URL]"
+		thumbnailing_service: "http://api.miniature.io/?width=500&height=400&screen=1280&url=[URL]"
 	};
 
 	// Creates default localStorage values if they don't already exist
@@ -80,7 +80,7 @@ function createDefaults() {
 
 	// TODO remove at some point, this will convert everyone to the new thumbnailing API
 	if (localStorage.getItem("thumbnailing_service").indexOf("immediatenet.com") > 0) {
-		localStorage.setItem("thumbnailing_service", "http://api.webthumbnail.org/?width=500&height=400&screen=1280&url=[URL]");
+		localStorage.setItem("thumbnailing_service", "http://api.miniature.io/?width=500&height=400&screen=1280&url=[URL]");
 	}
 }
 
